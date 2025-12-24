b9f8bb466fd81893a2e61687bbcb7888efa2e610
Afnaniagq <165085097+Afnaniagq@users.noreply.github.com>
2025-12-24 22:16:02 +0500

    fix: set correct baseUrl for production

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physical-ai-humanoid-robotics-book-one-blush.vercel.app/',
+  url: 'https://physical-ai-humanoid-robotics-book-bay.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
